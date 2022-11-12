80584b82bf6257728a176be9afe6a4d2f42bfe55
Yli <salazarylizanicole@gmail.com>
2022-11-12 12:05:30 +0800

    adding instructions

---
+++ add-search.py
@@ -0,0 +1,16 @@
+#Write a python program for contact tracing:
+# - Display a menu of options
+#Menu:
+# 1 -> Add an item
+# 2 -> Search
+# 3 -> Exit (y/n)
+# - Allow user to select item in the menu (check if valid)
+# - Perform the selected option
+# - Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+# Use dictionary to store the info
+# Use the full name as key
+# The value is another dictionary of personal information
+# - Option 2: Search, ask full name then display the record
+# - Option 3: Ask the user if want to exit or retry.
+
+#trying to commit a single item
