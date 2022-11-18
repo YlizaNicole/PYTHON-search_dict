5bdb148b4268316ace570c877316eefd7909acce
Yli <salazarylizanicole@gmail.com>
2022-11-18 16:25:54 +0800

    late realization of the instructions

--- add-search.py
+++ add-search.py
@@ -1,62 +1,48 @@
 #Write a python program for contact tracing:
-
-import shelve
-import dbm 
-
-# Use dictionary to store the info
-# Use the full name as key
-def save_dict(to_save):  
-    with shelve.open("shelve2.db", "c") as s: 
-                s["Dict"] = to_save  
-def load_dict():  
-        with shelve.open("shelve2.db", "r") as s:  
-                saved_dict = s["Dict"]  
-                return saved_dict   
-dict = load_dict() 
-data= list(dict.values())
-
-
+dict={}
 # - Display a menu of options
-Menu= print("MENU")
-print ("1 = Add an Item")
-print ("2 = Search")
-print ("3 = Exit")
+Menu= print("========MENU========")
+print ("| [1] = Add an Item |")
+print ("| [2] = Search      |")
+print ("| [3] = Exit        |")
+print("====================")
 
 # - Allow user to select item in the menu (check if valid)
 
-# - Perform the selected option
-# - Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+
 while True:
     print()
     user_input= int(input("pick a number: "))
-    if user_input ==1: 
-        ask = input("add item(y/n): ") 
-        if ask == "y":
-                keywords = input("full name: ")
-                Values = ("Full Name")
-                keywords = ("age")
-                Values = input("Age:")
-                keywords = ("Address: ")
-                Values = input("Address:")
-                keywords = ("Email: ")
-                Values = input("Email:")
-                dict[keywords] = Values
-                save_dict(dict)
-        elif ask == "n":
-            print(dict)  
+    # - Perform the selected option
+ # - Option 1: Ask personal data for contact tracing (Listed are sample only, add more)
+    if user_input ==1:
+        print("Type your Details")
+        FullName= input( "Full Name: ")
+        details={ # Use dictionary to store the info
+            FullName:{ # Use the full name as key
+                "age": int(input("age: ")),
+                "adress": input("adress: "),
+                "number": int(input("number: "))
+            }
+            }
         print("SAVED!")
-        
+        dict.update(details)
+# - Perform the selected option
 #Option 2: Search, ask full name then display the record
     if user_input ==2:
-        search= input("Enter Your name to search for your infos:  ")
-        print(dict.get(search))
-        print(dict.get("Age"))
-        print(dict.get("Address"))
-        print(dict.get("Email"))
+        print("Input the Full Name to search for the details")
+        search= input("Full Name:")
+        age = (dict[search]["age"])
+        print ("age:", age)
+        adress= (dict[search]["adress"])
+        print ("adress:", adress)
+        num = (dict[search]["number"])
+        print("number:", num)
+
+# - Perform the selected option
 #- Option 3: Ask the user if want to exit or retry.
     if user_input==3:
         question= input("exit? (y/n)")
         if question == "y":
-            print("bye bye ")
+            print("bye bye :>")
         break
-
